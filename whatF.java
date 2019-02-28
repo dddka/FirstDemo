cfad3ba5ae856775ad650971083705712ac1f8de
shanzd <37695848+dddka@users.noreply.github.com>
2019-02-28 19:56:49 +0800

    Add files via upload

---
+++ whatF.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.<p>
+ *
+ * @Auther: shanbaojia
+ * @Date: 2019/2/28 15:50
+ * @Description:
+ */
+public class whatF {
+
+
+
+    public  static void main (String[] args) throws InterruptedException {
+        final int i=0;
+        final HashMap<String, String> map = new HashMap<String, String>(2);
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            map.put(UUID.randomUUID().toString(),"");
+
+                            System.out.println(0);
+                        }
+                    }, "moon" + i).start();
+                }
+            }
+        }, "ftf");
+     t.start();
+     t.join();
+    }
+}
